2810eca7dbac1c7985f9f28f2a8e4bf604befe9f
Ciro Santilli <ciro.santilli@gmail.com>
2015-11-26 14:32:58 +0100

    sqlite

--- datetime_cheat.py
+++ datetime_cheat.py
@@ -10,19 +10,32 @@ import datetime
 
 now = datetime.datetime.now()
 print('now = ' + str(now))
-# timedelta(0)
-print('now - now = ' + str(now - now))
-# One day.
-print(now - datetime.timedelta(1))
-print(now - datetime.timedelta(
-    #years       = 1, # Not a valid argument.
-    weeks        = 2,
-    days         = 3,
-    hours        = 4,
-    minutes      = 5,
-    seconds      = 6,
-    milliseconds = 7,
-    microseconds = 8
-))
-    #get a datetime from a seconds after 1970 time.time()
-print(datetime.datetime.fromtimestamp(0))
+
+if '## timedelta':
+
+    # Time differences are of timedelta class.
+
+    diff = now - now
+    assert type(diff) == datetime.timedelta
+    assert diff == datetime.timedelta()
+
+    # One day.
+
+    date = datetime.datetime(2000, 1, 2)
+    assert date + datetime.timedelta(1) == datetime.datetime(2000, 1, 3)
+
+    print('timedelta full costructor = ' + str(datetime.timedelta(
+        # Not possible:
+        #years       = 1,
+        #months      = 1,
+        weeks        = 2,
+        days         = 3,
+        hours        = 4,
+        minutes      = 5,
+        seconds      = 6,
+        milliseconds = 7,
+        microseconds = 8
+    )))
+
+    # Get a datetime from a seconds since Epoch.
+    print('epoch = ' + str(datetime.datetime.fromtimestamp(0)))
---
+++ interactive-friendly.md
@@ -0,0 +1,9 @@
+# Interactive friendly
+
+Python did not replace Bash because it is too verbose for quick and dirty CLI operations.
+
+That is the price of sanity.
+
+There are a few projects which try to make Python golf better, at the price of some extra insanity:
+
+- <https://github.com/russell91/pythonpy>
